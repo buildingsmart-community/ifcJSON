3479fad7329ed4f0be138e5f4340ad7fd43ff9a7
Jan Brouwer <jbrouwer@Digibase.nl>
2021-05-26 21:10:31 +0200

    Fix Args inverse #8

--- file_converters/ifc2json.py
+++ file_converters/ifc2json.py
@@ -77,7 +77,7 @@ if __name__ == '__main__':
         if not args.v or args.v == "4":
             jsonData = ifcjson.IFC2JSON4(ifcFilePath,
                                          COMPACT,
-                                         NO_INVERSE=args.inverse,
+                                         NO_INVERSE=args.no_inverse,
                                          EMPTY_PROPERTIES=args.empty_properties,
                                          NO_OWNERHISTORY=args.no_ownerhistory,
                                          GEOMETRY=GEOMETRY
